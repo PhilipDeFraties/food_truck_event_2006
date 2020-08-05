a9d56439c2cd182d1ae74236b779ee839a49a7df
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-05 10:12:56 -0600

    Add potential_revenue method.

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class FoodTruck
   attr_reader :name,
               :inventory
@@ -20,6 +21,12 @@ class FoodTruck
       @inventory[item] += quantity
     else
       @inventory[item] = quantity
+    end
+  end
+
+  def potential_revenue
+    @inventory.sum do |item|
+      item[0].price.delete("$").to_f * item[1]
     end
   end
 end
