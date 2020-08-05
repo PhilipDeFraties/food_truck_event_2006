ca7e9109ef8aeaa0d5783ba52f50cb5bf945f031
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-05 09:33:36 -0600

    Add inventory, stock, check stock methods.

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -7,5 +7,19 @@ class FoodTruck
     @inventory = {}
   end
 
+  def check_stock(item)
+    if !@inventory[item]
+      0
+    else
+      @inventory[item]
+    end
+  end
 
+  def stock(item, quantity)
+    if @inventory[item]
+      @inventory[item] += quantity
+    else
+      @inventory[item] = quantity
+    end
+  end
 end
--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -18,5 +18,28 @@ class FoodTruckTest < MiniTest::Test
     assert_equal ({}), food_truck.inventory
   end
 
-  def
+  def test_item_is_not_stocked_by_default
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    assert_equal 0, food_truck.check_stock(item1)
+  end
+
+  def test_it_can_stock_item
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck.stock(item1, 30)
+
+    assert_equal 30, food_truck.check_stock(item1)
+    assert_equal ({item1 => 30}), food_truck.inventory
+
+    food_truck.stock(item1, 25)
+
+    assert_equal 55, food_truck.check_stock(item1)
+
+    food_truck.stock(item2, 12)
+
+    assert_equal ({item1 => 55, item2 => 12}), food_truck.inventory
+  end
 end
